7bd990ed9dd9da1ecb746b4d4e1bfbd8a7acf337
Jordan Aitken <Jordantfa@hotmail.co.uk>
2019-08-20 14:58:44 +0100

    Initial Commit

---
+++ Wazda/Wazda-Child/src/test/java/HelloWorldTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+
+public class HelloWorldTest {
+
+    @Test
+    public void shouldAnswerWithTrue(){
+        assertTrue(true);
+    }
+}
