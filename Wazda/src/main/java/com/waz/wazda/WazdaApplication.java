e1285bf241ccdd2c9dd30281cb9516e35584834f
Jordan Aitken <JordanTFA@hotmail.co.uk>
2020-07-26 16:49:33 +0100

    Creating Spring setup

---
+++ Wazda/src/main/java/com/waz/wazda/WazdaApplication.java
@@ -0,0 +1,13 @@
+package com.waz.wazda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WazdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WazdaApplication.class, args);
+	}
+
+}
